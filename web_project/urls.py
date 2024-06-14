34a9335c6afd78deaf463e14eafd9310c5d86c8a
Pedroliu666 <Pedroliu666@github.com>
2024-06-14 13:53:06 +0800

    Final Details Done

--- web_project/urls.py
+++ web_project/urls.py
@@ -24,7 +24,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),  # API endpoints
-    
     # Serve React's index.html for all other routes
     re_path(r"^(?!api/).*$", TemplateView.as_view(template_name="index.html")),
 ]
